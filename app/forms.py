8d849c0b0a7bc6f5de19a389ac380f5a8819843c
wael159 <50086578+wael159@users.noreply.github.com>
2020-10-30 19:05:21 +0200

    shop improve

--- app/forms.py
+++ app/forms.py
@@ -10,7 +10,7 @@ class SignupForm(flask_wtf.FlaskForm):
 
 class searchquery(flask_wtf.FlaskForm):
 
-    searchby = wtforms.SelectField("search method:", choices=[('name', 'name'), ('category', 'category'),('id', 'id')],validators=[vld.DataRequired()])
+    searchby = wtforms.SelectField("search method:", choices=[('name', 'name'), ('category', 'category'),('id', 'id'),('author_name', 'author_name'),('year','year')],validators=[vld.DataRequired()])
     value = wtforms.StringField("value:", validators=[vld.DataRequired()])
     submit   = wtforms.SubmitField("Search")
 
@@ -22,3 +22,13 @@ class SigninForm(flask_wtf.FlaskForm):
     submit   = wtforms.SubmitField("Sign in")
 
 
+class Add_books(flask_wtf.FlaskForm):
+
+    name = wtforms.StringField("Name:", validators=[vld.DataRequired()])
+    price = wtforms.StringField("Price:", validators=[vld.DataRequired()])
+    stock_quantity   = wtforms.StringField("stock quantity")
+    category=wtforms.StringField("Category:", validators=[vld.DataRequired()])
+    description = wtforms.StringField("description:", validators=[vld.DataRequired()])
+    author_name = wtforms.StringField("author_name:", validators=[vld.DataRequired()])
+    year = wtforms.StringField("year:", validators=[vld.DataRequired()])
+    submit=wtforms.SubmitField("Submit")
