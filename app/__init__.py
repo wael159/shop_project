591daac190e4fc690de5597626ec1e4b63035e58
wael159 <50086578+wael159@users.noreply.github.com>
2020-11-01 14:30:51 +0200

    shop improve

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,24 @@
 from flask import Flask
 from config import Config
 
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+#from flask_sqlalchemy import SQLAlchemy
+#from flask_migrate import Migrate
 from flask_login import LoginManager
+import flask_sqlalchemy
+import flask_migrate
 
-db  = SQLAlchemy()
-migrate = Migrate()
+#db  = SQLAlchemy()
+#migrate = Migrate()
 login_mgr = LoginManager()
 
 app=Flask(__name__)
 app.config.from_object(Config)
 
-db.init_app(app)
-migrate.init_app(app, db)
+#db.init_app(app)
+#migrate.init_app(app, db)
+
+db      = flask_sqlalchemy.SQLAlchemy(app)
+migrate = flask_migrate.Migrate(app, db)
 login_mgr.init_app(app)
 
 
--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 class Config:
     #FORMAT: "postgres://username:password@server_address:server_port/database"
-    SQLALCHEMY_DATABASE_URI = "postgres://postgres:159753852@localhost:5432/my_first_database"
+    #SQLALCHEMY_DATABASE_URI = "postgres://postgres:159753852@localhost:5432/my_first_database"
     # It has to be called SQLALCHEMY_DATABASE_URI
+    import os
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir, 'app.db')
     SECRET_KEY = "chocolate"
